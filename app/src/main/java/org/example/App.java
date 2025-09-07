af759110125d14328dd0d25b6f37a4e9c82688f0
MicrowaveHoddie <fightingchristian132@gmail.com>
2025-09-07 15:56:38 -0500

    fixed exit command

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,47 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
-    // Implement your function here
-  }
+    Scanner scanner = new Scanner(System.in);
+
+    public int getInput(String prompt, String alert, int low, int high) {
+        int value;
+
+        while (true) {
+            System.out.println(prompt);
+            String input = scanner.nextLine();
+
+            try {
+                String lowerCaseInput = input.toLowerCase();
+                if (lowerCaseInput.equals("default")){
+                    value = 100;
+                } else if (lowerCaseInput.equals("exit")) {
+                    value = -1;
+                  return value;
+                } else {
+                    value = Integer.parseInt(lowerCaseInput);
+                }
+
+                if (value >= low && value <= high) {
+                    return value;
+                } else {
+                    System.out.println(alert);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number.");
+            }
+        }
+    }
 
   public static void main(String[] args) {
     // Test your function here in a driver program
+      App app = new App();
+      int result = app.getInput("Enter a number between 1 and 100, \"default\" for default value of 100, or \"exit\" to abort operation", "ERROR! That is not a valid input!", 0, 100);
+      if (!(result == -1)) {
+          System.out.println("You have inputted: " + result);
+      }
+      System.out.println("Goodbye!");
   }
 }
